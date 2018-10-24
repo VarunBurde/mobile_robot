a078f932533041bb4292a80a0aa8a61f20ace65e
student <student@desktop-03.felk.cvut.cz>
2018-10-24 14:54:24 +0200

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/varunmk_ws/src
+# Build directory: /home/student/varunmk_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(simulator_e130)
+subdirs(robot_coordination)
---
+++ build/robot_coordination/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/varunmk_ws/src/robot_coordination
+# Build directory: /home/student/varunmk_ws/build/robot_coordination
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
